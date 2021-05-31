5908b5bc011da2677b77f337e3d724792d79808a
Martin Sarabia <msarabia78@gmail.com>
2021-05-31 11:04:28 -0500

    [WIP] splash

--- app/src/main/java/com/msarabia/cartelera/MainActivity.kt
+++ app/src/main/java/com/msarabia/cartelera/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Cartelera)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
